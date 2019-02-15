5e6683c18bfcc9e9fd473fa828bce2307a90b94d
wusj7856 <wusj@realbull.net>
2019-02-15 11:48:13 +0800

    add FindGLFW3.cmake

---
+++ cmake/FindGLFW3.cmake
@@ -0,0 +1,38 @@
+message( "finding GLFW3!"  )
+
+if(WIN32)
+
+    message("Is Windows")
+    set(GLFW3_PATH $ENV{GLFW3_PATH})
+    if( GLFW3_PATH )
+
+        message("Find GLFW3_PATH env!")
+        message(${GLFW3_PATH})
+
+        find_path( GLFW3_INCLUDE_DIR glfw3.h "${GLFW3_PATH}/include/GLFW" )
+        find_library( GLFW3_LIBRARY glfw3.lib "${GLFW3_PATH}/lib" "${GLFW3_PATH}/lib-vc2015")
+
+        if( GLFW3_INCLUDE_DIR AND GLFW3_LIBRARY)
+
+            set( GLFW3_FOUND TRUE )
+
+        else()
+
+            set( GLFW3_FOUND FALSE )
+
+        endif()
+
+    else()
+
+        set( GLFW3_FOUND FALSE )
+        message("Not Find GLFW3_PATH env!")
+
+    endif()
+
+else()
+
+    message("Not Windows!")
+
+endif()
+
+message("................................................................")
