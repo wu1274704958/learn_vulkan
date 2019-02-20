987921844b6ab0ac59fc559ab212c0220b594f62
wu1274704958 <1274704958@qq.com>
2019-02-20 18:57:15 +0800

    triangle .

--- triangle/main.cpp
+++ triangle/main.cpp
@@ -5,6 +5,12 @@
 #include <vulkan/vulkan.h>
 #include <vulkanexamplebase.h>
 
+struct Vertex
+{
+	float position[3];
+	float color[3];
+};
+
 class Triangle : public VulkanExampleBase
 {
 public:
@@ -14,9 +20,45 @@ public:
 
 	Triangle() : VulkanExampleBase(false)
 	{
-
+		zoom = -2.5f;
+		title = "triangle";
 	}
 private:
+
+	struct {
+		VkDeviceMemory memory;
+		VkBuffer buffer;
+	} vertices;
+
+	struct {
+		VkDeviceMemory memory;
+		VkBuffer buffer;
+		uint32_t count;
+	} indeices;
+
+	struct {
+		VkDeviceMemory memory;
+		VkBuffer buffer;
+		VkDescriptorBufferInfo descriptor;
+	} uniformBufferVS;
+
+	struct {
+		glm::mat4x4 projectionMatrix;
+		glm::mat4x4 modelMatrix;
+		glm::mat4x4 viewMatrix;
+	} uboVS;
+
+	VkPipelineLayout pipelineLayout;
+	VkPipeline pipeline;
+	VkDescriptorSetLayout descriptorSetLayout;
+	VkDescriptorSet descriptorSet;
+	
+	//用于协调图形队列中的操作并确保正确的命令顺序
+	VkSemaphore presentSemaphore;
+	VkSemaphore renderSemaphore;
+
+	//用于检查队列操作的完成情况（例如命令缓冲区执行）
+	std::vector<VkFence> waitFences;
 
 };
 
