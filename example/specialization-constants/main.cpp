e67397e311892ea55de6a523d11a4b55235057cd
wu1274704958 <1274704958@qq.com>
2019-03-19 16:55:10 +0800

    add specialization-constants

---
+++ example/specialization-constants/main.cpp
@@ -0,0 +1,78 @@
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vulkan/vulkan.h>
+#include <vulkanexamplebase.h>
+#include <VulkanBuffer.hpp>
+#include <VulkanDevice.hpp>
+#include <comm/CommTool.hpp>
+#include <VulkanModel.hpp>
+#include <comm/dbg.hpp>
+
+class Example : public VulkanExampleBase {
+public:
+	virtual void render() override
+	{
+
+	}
+
+	Example() : VulkanExampleBase(true)
+	{
+
+	}
+
+	~Example() {
+
+	}
+};
+
+#if defined(_WIN32)
+
+Example *example;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (example != NULL)
+	{
+		example->handleMessages(hWnd, uMsg, wParam, lParam);
+	}
+	return (DefWindowProc(hWnd, uMsg, wParam, lParam));
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+	/*for (size_t i = 0; i < __argc; i++) { Example::args.push_back(__argv[i]); };
+	example = new Example();
+	example->initVulkan();
+	example->setupWindow(hInstance, WndProc);
+	example->prepare();
+	example->renderLoop();
+	delete example;*/
+	dbg("sss");
+	system("pause");
+	return 0;
+}
+
+#elif defined(__linux__)
+
+// Linux entry point
+Example *example;
+static void handleEvent(const xcb_generic_event_t *event)
+{
+	if (example != NULL)
+	{
+		example->handleEvent(event);
+	}
+}
+int main(const int argc, const char *argv[])
+{
+	for (size_t i = 0; i < argc; i++) { Example::args.push_back(argv[i]); };
+	example = new Example();
+	example->initVulkan();
+	example->setupWindow();
+	example->prepare();
+	example->renderLoop();
+	delete example;
+	return 0;
+}
+#endif
