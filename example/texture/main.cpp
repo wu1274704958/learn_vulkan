3d92eac41da881f93c45bf161daee95aec7d9f6e
wu1274704958 <1274704958@qq.com>
2019-04-19 18:14:32 +0800

    texture [-]

--- example/texture/main.cpp
+++ example/texture/main.cpp
@@ -9,8 +9,44 @@
 #include <comm/dbg.hpp>
 #include <VulkanTexture.hpp>
 
+struct Vertex {
+	glm::vec3 pos;
+	glm::vec2 uv;
+	glm::vec3 normal;
+};
+
 class Example : public VulkanExampleBase {
 private:
+
+	struct Texture {
+		VkSampler sampler;
+		VkImage image;
+		VkImageLayout imageLayout;
+		VkDeviceMemory memory;
+		VkImageView imageView;
+		uint32_t width, height, mipLevels;
+	} texture;
+
+	struct {
+		glm::mat4 projection;
+		glm::mat4 model;
+		glm::vec4 viewPos;
+		float lodBias = 0.0f;
+	} uboVS;
+	
+	vks::Buffer vertexBuffer;
+	vks::Buffer indexBuffer;
+	vks::Buffer uniformBuffer;
+
+	uint32_t indexCount;
+
+	struct {
+		VkPipeline soild;
+	} pipelines;
+
+	VkDescriptorSetLayout  descriptorSetLayout;
+	VkPipelineLayout pipelineLayout;
+	VkDescriptorSet descriptorSet;
 
 public:
 	void render() override
@@ -20,13 +56,104 @@ public:
 
 	Example() : VulkanExampleBase(true)
 	{
-		title = "texture";
+		zoom = -2.5f;
+		title = "load texture";
+		rotation = { 0.0f, 15.0f, 0.0f };
+		settings.overlay = true;
 	}
 
 	~Example()
 	{
+		uniformBuffer.destroy();
+		vertexBuffer.destroy();
+		indexBuffer.destroy();
+
+		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+		vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
+		vkDestroyPipeline(device, pipelines.soild, nullptr);
+		
+		destroyTextureImage(texture);
+	}
+
+	void destroyTextureImage(Texture &texture)
+	{
+		vkDestroyImageView(device, texture.imageView,nullptr);
+		vkDestroyImage(device,texture.image,nullptr);
+		vkDestroySampler(device, texture.sampler, nullptr);
+		vkFreeMemory(device, texture.memory, nullptr);
+	}
+
+	void getEnabledFeatures() override
+	{
+		if (deviceFeatures.samplerAnisotropy)
+		{
+			enabledFeatures.samplerAnisotropy = VK_TRUE;
+		}
+	}
+
+	void loadTexture()
+	{
+		std::string path = getAssetPath() + "textures/metalplate01_rgba.ktx";
+		VkFormat format = VK_FORMAT_R8G8B8A8_UNORM;
+
+#if defined(__ANDROID__)
+		// Textures are stored inside the apk on Android (compressed)
+		// So they need to be loaded via the asset manager
+		AAsset* asset = AAssetManager_open(androidApp->activity->assetManager, path.c_str(), AASSET_MODE_STREAMING);
+		assert(asset);
+		size_t size = AAsset_getLength(asset);
+		assert(size > 0);
+
+		void *textureData = malloc(size);
+		AAsset_read(asset, textureData, size);
+		AAsset_close(asset);
+
+		gli::texture2d tex2D(gli::load((const char*)textureData, size));
+#else
+		gli::texture2d tex2D(gli::load(path));
+#endif
+		assert(!tex2D.empty());
+
+		texture.mipLevels = static_cast<uint32_t>(tex2D.levels);
+		texture.width = static_cast<uint32_t>(tex2D[0].extent().x);
+		texture.height = static_cast<uint32_t>(tex2D[0].extent().y);
+
+		bool useStaging = true;
+
+		bool forceLinearTiling = false;
+		if (forceLinearTiling)//如果强制启用线性平铺 如果设备支持就不用 device local memory
+		{
+			VkFormatProperties properties;
+			vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &properties);
+			useStaging = !(properties.linearTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT);
+		}
+
+		using namespace vks::initializers;
+
+		VkMemoryAllocateInfo memAllocInfo = memoryAllocateInfo();
+		VkMemoryRequirements memRequirments;
+
+		if (useStaging)
+		{
+			VkBuffer stagingBuffer;
+			VkDeviceMemory stagingMem;
+
+			VkBufferCreateInfo bufferCI = bufferCreateInfo(VK_BUFFER_USAGE_TRANSFER_SRC_BIT, tex2D.size());
+			bufferCI.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+			vkCreateBuffer(device, &bufferCI, nullptr, &stagingBuffer);
+
+
+		}
+		else {
+
+		}
+	}
+
+	void buildCommandBuffers() override
+	{
 
 	}
+
 };
 
 #if defined(_WIN32)
