86cf2edbada40d50914a5c9b838a32dbb57ef850
wu1274704958 <1274704958@qq.com>
2019-09-14 21:10:53 +0800

    add EDF_EXAMPLE_MAIN_FUNC macro

---
+++ comm/macro.h
@@ -0,0 +1,65 @@
+#pragma once
+
+
+#if defined(_WIN32)
+
+#define EDF_EXAMPLE_MAIN_FUNC(Example,is_test,...)							\
+Example* example;															\
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)\
+{																			\
+	if (example != NULL)													\
+	{																		\
+		example->handleMessages(hWnd, uMsg, wParam, lParam);				\
+	}																		\
+	return (DefWindowProc(hWnd, uMsg, wParam, lParam));						\
+}																			\
+																			\
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)	\
+{																									\
+	if constexpr(!is_test)																			\
+	{																								\
+		for (size_t i = 0; i < __argc; i++) { Example::args.push_back(__argv[i]); };				\
+		example = new Example();																	\
+		example->initVulkan();																		\
+		example->setupWindow(hInstance, WndProc);													\
+		example->prepare();																			\
+		example->renderLoop();																		\
+		delete example;																				\
+	}																								\
+	else {																							\
+		__VA_ARGS__																					\
+	}																								\
+	return 0;																						\
+}																									\
+
+#elif defined(__linux__)
+
+#define EDF_EXAMPLE_MAIN_FUNC(Example,is_test,...)													\
+Example* example;																					\
+static void handleEvent(const xcb_generic_event_t* event)											\
+{																									\
+	if (example != NULL)																			\
+	{																								\
+		example->handleEvent(event);																\
+	}																								\
+}																									\
+int main(const int argc, const char* argv[])														\
+{																									\
+	if constexpr(is_test)																			\
+	{																								\
+		for (size_t i = 0; i < argc; i++) { Example::args.push_back(argv[i]); };					\
+		example = new Example();																	\
+		example->initVulkan();																		\
+		example->setupWindow();																		\
+		example->prepare();																			\
+		example->renderLoop();																		\
+		delete example;																				\
+	}																								\
+	else {																							\
+		__VA_ARGS__																					\
+	}																								\
+	return 0;																						\
+}																									
+#endif
+
+
--- example/texture3d/main.cpp
+++ example/texture3d/main.cpp
@@ -12,6 +12,7 @@
 #include <comm/dbg.hpp>
 #include <random>
 #include <VulkanModel.hpp>
+#include "comm/macro.h"
 
 
 //#define FRACTAL
@@ -669,58 +670,9 @@ public:
 	}
 };
 
-
-
-#if defined(_WIN32)
-
-Example *example;
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    if (example != NULL)
-    {
-        example->handleMessages(hWnd, uMsg, wParam, lParam);
-    }
-    return (DefWindowProc(hWnd, uMsg, wParam, lParam));
-}
-
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
-{
-    for (size_t i = 0; i < __argc; i++) { Example::args.push_back(__argv[i]); };
-    example = new Example();
-    example->initVulkan();
-    example->setupWindow(hInstance, WndProc);
-    example->prepare();
-    example->renderLoop();
-    delete example;
-	/*auto perNoise = PerlinNoise<float>();
-	auto res = perNoise.noise(0.5f, 0.5f, 0.0f);
-	std::string r;
-	r.reserve(200);
-	sprintf(r.data(), "res = %f", res);
-	MessageBoxA(0, r.data(), "tip", 0);*/
-    return 0;
-}
-
-#elif defined(__linux__)
-
-// Linux entry point
-Example *example;
-static void handleEvent(const xcb_generic_event_t *event)
-{
-	if (example != NULL)
-	{
-		example->handleEvent(event);
-	}
-}
-int main(const int argc, const char *argv[])
-{
-	for (size_t i = 0; i < argc; i++) { Example::args.push_back(argv[i]); };
-	example = new Example();
-	example->initVulkan();
-	example->setupWindow();
-	example->prepare();
-	example->renderLoop();
-	delete example;
-	return 0;
-}
-#endif
+EDF_EXAMPLE_MAIN_FUNC(Example, false, {
+	int a = 1;
+	char buf[9] = {0};
+	sprintf(buf,"hhh%d",a);
+	MessageBoxA(NULL, buf, "Tip", MB_OK);
+})
